0ca56b3febca3ece711f18e649410ac3f9162282
Lucdomingues <lojalucc@outlook.com>
2023-03-27 19:04:10 -0300

    [feat]-Requisito-01

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,16 @@
+// import Race from "./Race";
+
+// export default class Dwarf extends Race {
+//   private static instancescCount: number = 0
+//   constructor(
+//     name: string,
+//     dexterity: number,
+//     override maxLifePoints: number = 80) {
+//     super(name, dexterity)
+//     Dwarf.createdRacesInstances()
+//   }
+
+//   static createdRacesInstances(): number {
+//       return Dwarf.instancescCount += 1
+//   }
+// }
